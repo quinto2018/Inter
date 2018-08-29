50da3757795897df2d5aa7ddde135a5cc1317269
lukenzera <lucas.azevedo2104@gmail.com>
2018-08-29 12:28:09 -0300

    Add 1 test
    Add test

---
+++ Inter2018/Assets/Só teste.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sóteste : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// This is a test moda foka
+
+		
+	}
+}
